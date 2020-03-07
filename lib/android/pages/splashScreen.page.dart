2a35b941ba046b3422600f1977b398a05d56fd30
Eliezer Castro <eliezergino@gmail.com>
2020-03-07 00:15:03 -0300

    bug gradient

--- lib/android/pages/splashScreen.page.dart
+++ lib/android/pages/splashScreen.page.dart
@@ -12,7 +12,7 @@ class _SplashScreenState extends State<SplashScreen> {
       body: SingleChildScrollView(
         child:// First Page 
           Container(
-            width: 411,
+            width: double.infinity,
             height: 731,
             decoration: BoxDecoration(
                   gradient: LinearGradient(
